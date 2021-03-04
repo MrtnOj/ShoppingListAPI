3c755fd5fe6d60ef856a41539c6d3e4f33276e17
Martin Ojasalu <ojasalum@gmail.com>
2021-03-04 03:38:25 +0200

    item create model fixed

--- src/models/item.ts
+++ src/models/item.ts
@@ -40,25 +40,3 @@ const Item = sequelize.define('item', {
 })
 
 export default Item
-
-
-
-// export class Item implements ItemInterface {
-//     id: number;
-//     name: string;
-//     categoryId?: number;
-//     lasts?: number;
-//     lastBought?: Date 
-
-//     constructor(id: number, name: string, categoryId?: number, lasts?: number, lastBought?: Date) {
-//         this.id = id;
-//         this.name = name;
-//         this.categoryId = categoryId;
-//         this.lasts = lasts;
-//         this.lastBought = lastBought;
-//     }
-
-//     static fetchAll() {
-//         return db.query('SELECT * FROM items')
-//     }
-// }

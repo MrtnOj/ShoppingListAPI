367ef1db017873935317efdb9af283dbe444ceee
Martin Ojasalu <ojasalum@gmail.com>
2021-03-20 22:31:28 +0200

    username restrictions added

--- dist/routes/auth.js
+++ dist/routes/auth.js
@@ -15,15 +15,25 @@ router.post('/signup', [
         .custom(value => {
         return user_1.default.findOne({ where: { email: value } })
             .then(user => {
-            console.log(user);
             if (user) {
                 return Promise.reject('Email address already exists');
-                // throw error for handling
             }
         });
     })
         .withMessage('Email address already exists')
         .normalizeEmail(),
+    express_validator_1.body('username')
+        .trim()
+        .isLength({ min: 2 })
+        .withMessage('Username must be at least 2 characters long')
+        .custom(value => {
+        return user_1.default.findOne({ where: { username: value } })
+            .then(user => {
+            if (user) {
+                return Promise.reject('Username is already taken');
+            }
+        });
+    }),
     express_validator_1.body('password')
         .trim()
         .isLength({ min: 6 })
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -12,15 +12,25 @@ router.post('/signup', [
         .custom(value => {
            return User.findOne({ where: { email: value} })
             .then(user => {
-                console.log(user)
                 if (user) {
                     return Promise.reject('Email address already exists')
-                    // throw error for handling
                 }
             })
         })
         .withMessage('Email address already exists')
         .normalizeEmail(),
+    body('username')
+        .trim()
+        .isLength({ min: 2 })
+        .withMessage('Username must be at least 2 characters long')
+        .custom(value => {
+            return User.findOne({ where: {username: value}})
+            .then(user => {
+                if (user) {
+                    return Promise.reject('Username is already taken')
+                }
+            })
+        }),
     body('password')
         .trim()
         .isLength({ min: 6 })
